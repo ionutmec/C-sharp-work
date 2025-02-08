3a5716fb9ab33aafa607832d996c9c4dc6b1f4e5
ionutmec <ionutmecu@gmail.com>
2025-02-08 03:06:22 +0000

    new

---
+++ OperatorsAssignmentSubmission/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignmentSubmission
+{
+    //Created a class called Employee and give it two properties, FirstName and LastName
+    public class Employee
+    {
+        public string firstName {  get; set; }
+        public string lastName { get; set; }
+        //In the Employee class, overload the “==” operator so it checks if two Employee
+        //objects are equal by comparing their Id property
+        public firstName 
+        {
+            John, Alex, Brad, 
+        }
+        bool isEmployee = false;
+
+
+
+
+
+    }
+}
