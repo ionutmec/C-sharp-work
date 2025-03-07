1d7d602c01c9d4f316bc7c07ceb14f39a256c738
ionutmec <ionutmecu@gmail.com>
2025-03-07 12:20:13 +0000

    new

--- TwentyOne/Deck.cs
+++ TwentyOne/Deck.cs
@@ -11,24 +11,38 @@ namespace TwentyOne
         public Deck()
         {
             Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
-            List<string> Faces = new List<string>()
-            {
-                "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Jack", "Qeen", "King", "Ace"
-            };
 
-            foreach (string face in Faces)
+            for (int i = 0; i < 13; i++)
             {
-                foreach (string suit in Suits)
+                for (int j = 0; j < 4; j++)
                 {
-                    Card card =new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
                     Cards.Add(card);
                 }
             }
         }
-        public List<Card> Cards {  get; set; }
+
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+
+        }
     }
 }
+
